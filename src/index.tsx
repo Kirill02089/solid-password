bffbf657c96ba288d89e80ab63542e322e347614
Kirill Romanov <kirillromanov70@gmail.com>
2023-01-21 15:08:03 +0400

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'solid-js/web'
 
 import './index.css'
 import App from './App'
+import './service-worker'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [solidPlugin(), VitePWA()],
     server: {
         port: 3000,
     },
     build: {
-        target: 'esnext',
+        target: 'esnext'
     },
 })
